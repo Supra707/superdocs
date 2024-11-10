845dc55e403ba82a1dc8fd700061ee4e3b6e5a49
Supratik De <supratikde2@gmail.com>
2024-11-11 00:50:37 +0530

    dahsboard ui and database integration done

--- components/modalcontent.jsx
+++ components/modalcontent.jsx
@@ -2,25 +2,37 @@
 import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { Button } from "@nextui-org/button";
-import { ReloadIcon } from "@radix-ui/react-icons";
-const ModalContent = ({ doc, setOpen, setdoc, setok }) => {
+import { collection, addDoc,serverTimestamp } from "firebase/firestore"; 
+import { auth,db } from "@/firebase.config";
+const ModalContent = ({ docu, setOpen, setdoc, setok }) => {
   const [loading, setloading] = useState(false);
   const handleOnChange = (e) => {
     setdoc(e.target.value);
   };
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault();
-    if (doc === "") {
+    if (docu === "") {
       toast.error("Document Name cannot be Empty");
       return;
     }
     setloading(true);
-    setTimeout(() => {
+    //passing data about the document to the firestore database//
+    try {
+      const docRef = await addDoc(collection(db, "Documents"), {
+        User:auth.currentUser.displayName,
+        Title:docu,
+        createdAt: serverTimestamp()
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+   
       toast.success("Document Created");
       setloading(false);
       setOpen(false);
       setok(true);
-    }, 2000);
+    
   };
   return (
     <div>
@@ -35,7 +47,7 @@ const ModalContent = ({ doc, setOpen, setdoc, setok }) => {
           </label>
           <input
             onChange={handleOnChange}
-            value={doc}
+            value={docu}
             type="email"
             id="email"
             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
